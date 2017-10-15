a066f0387112474104b1e8b313d683ed56f1cd71
ArtikusHG <artiomoleynic@gmail.com>
2017-10-15 16:39:11 +0300

    Moo

---
+++ cowsay.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+size=${#1}
+if [ "$size" -gt 30 ]; then
+echo 
+echo 
+echo " ________________________________"
+echo "< I can't say more than 30 chars >"
+echo " --------------------------------"
+echo "        \   ^__^"
+echo "         \  (oo)\_______"
+echo "            (__)\       )--"
+echo "                ||     ||"
+echo "                ||     ||"
+echo 
+echo 
+else
+echo 
+echo 
+echo " _______________________________"
+echo "< $1 >"
+echo " -------------------------------"
+echo "        \   ^__^"
+echo "         \  (oo)\_______"
+echo "            (__)\       )--"
+echo "                ||     ||"
+echo "                ||     ||"
+echo 
+echo 
+fi
